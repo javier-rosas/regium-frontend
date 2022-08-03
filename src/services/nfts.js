53e871e2972ad56c9c23afec4b58247d6131a5f0
DhruvSabharwal <sabharwal.d@northeastern.edu>
2022-08-02 23:56:41 -0400

    refactoring to nfts

---
+++ src/services/nfts.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+class NftDataService {
+  getAll(page = 0) {
+    return axios.get(
+      `${process.env.REACT_APP_API_BASE_URL}/api/v1/nfts?page=${page}`
+    );
+  }
+
+  find(query, by = "title", page = 0) {
+    if (by === "title") {
+      return axios.get(
+        `${process.env.REACT_APP_API_BASE_URL}/api/v1/nfts?title=${query}&page=${page}`
+      );
+    } else if (by === "id") {
+      return axios.get(
+        `${process.env.REACT_APP_API_BASE_URL}/api/v1/nfts/id/${query}`
+      );
+    } else if (by === "rated") {
+      return axios.get(
+        `${process.env.REACT_APP_API_BASE_URL}/api/v1/nfts?rated=${query}&page=${page}`
+      );
+    }
+  }
+
+  // getRatings() {
+  //     return axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/v1/nfts/ratings`)
+  // }
+
+  // createReview(data) {
+  //     return axios.post(`${process.env.REACT_APP_API_BASE_URL}/api/v1/nfts/review`, data)
+  // }
+
+  // updateReview(data) {
+  //     return axios.put(`${process.env.REACT_APP_API_BASE_URL}/api/v1/nfts/review`, data)
+  // }
+
+  // deleteReview(data) {
+  //     return axios.delete(`${process.env.REACT_APP_API_BASE_URL}/api/v1/nfts/review`, { data: data })
+  // }
+}
+
+export default new NftDataService();
